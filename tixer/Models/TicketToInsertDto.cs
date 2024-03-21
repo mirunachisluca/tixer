49632baa52b12bd474d4073a469debb5135511e6
Miruna Chisluca <miruna.chisluca@gmail.com>
2024-03-21 23:12:29 +0200

    update validation rules for tickets

--- tixer/Models/TicketToInsertDto.cs
+++ tixer/Models/TicketToInsertDto.cs
@@ -4,11 +4,12 @@ namespace Tixer.DTOs
 {
     public class TicketToInsertDto
     {
-        [Required]
-        [MaxLength(20)]
+        [Required(ErrorMessage = "Title cannot be empty")]
+        [MaxLength(150, ErrorMessage = "Title must not be greater than 150 characters")]
         public string Title { get; set; } = string.Empty;
 
         [Required]
+        [Range(0, 5000.0, ErrorMessage = "Price must be between than 0 and 5000")]
         public decimal Price { get; set; }
     }
 }
